e76d7acd9648c33f7995419927d71d38a2df7706
RAFEEK-S <rafeeksarthar73@gmail.com>
2024-03-25 16:24:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to React tutorial hi hello</h1>
-      </header>
+    <div>
+      <p>hi hello</p>
     </div>
   );
 }
