b62494a8876c0ca469de76d380e3f6c3c53e1768
RAFEEK-S <rafeeksarthar73@gmail.com>
2024-03-25 14:11:22 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to React tutorial hi</h1>
+        <h1>Welcome to React tutorial hi hello</h1>
       </header>
     </div>
   );
